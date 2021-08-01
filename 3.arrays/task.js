95759b7588ef47426a2c0afa830e071195404887
Kira667 <kkg80@yandex.ru>
2021-08-01 23:22:40 +0300

    3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((item, i) => {
+    return arr1[i] === arr2[i];
+  });
+  // for (let i = 0; i < arr1.length; i++) {
+  //   if (arr1[i] !== arr2[i]) {
+  //     return false;
+  //   }
+  // }
+  // return true;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.slice().filter(item => {
+    return item % 3 === 0 && item > 0;
+  }).map(item => item * 10);
 }
